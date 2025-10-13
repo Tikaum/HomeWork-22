a82ab29243e77f8bdf9a3e2f55a53f00996be1b5
tikaum <tulkubai@rambler.ru>
2025-10-13 15:30:59 +0500

    add screenshots

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -1,5 +1,7 @@
-﻿using Homework_22.Pages;
+﻿using Allure.Net.Commons;
+using Homework_22.Pages;
 using Homework_22.Utils;
+using NUnit.Framework.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,7 @@ namespace Homework_22.Tests
     public class BaseTest
     {
         LoginPage loginPage = new LoginPage();
+        ScreenshotUtils screenshotUtils = new ScreenshotUtils();
 
         [SetUp]
         public void Setup()
@@ -21,6 +24,19 @@ namespace Homework_22.Tests
         [TearDown]
         public void Teardown()
         {
+            var context = TestContext.CurrentContext;
+            if (context.Result.Outcome.Status == TestStatus.Failed)
+            {
+                AllureLifecycle.Instance.UpdateTestCase(x =>
+                {
+                    x.attachments.Add(new Attachment
+                    {
+                        name = "Failure Screenshot",
+                        type = "image/png",
+                        source = screenshotUtils.SaveScreenshotAndReturnFileName()
+                    });
+                });
+            }
             DriverManager.Quit();
         }
     }
